483902def62267c1297b9137d9948d2ede3bd302
mrm65 <minan.65@hotmail.com>
2022-05-14 01:39:19 +0300

    DefaultController

--- MvcWebUI/Controllers/DefaultController.cs
+++ MvcWebUI/Controllers/DefaultController.cs
@@ -17,5 +17,10 @@ namespace MvcWebUI.Controllers
         {
             return PartialView();
         }
+
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();
+        }
     }
 }
