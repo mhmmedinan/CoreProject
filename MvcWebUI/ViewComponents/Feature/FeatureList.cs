6b51137d7cbceeeca936307f343d660d6d0a8fb2
mrm65 <minan.65@hotmail.com>
2022-05-14 01:50:53 +0300

    FeautureList

---
+++ MvcWebUI/ViewComponents/Feature/FeatureList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWebUI.ViewComponents.Feature
+{
+    public class FeatureList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
