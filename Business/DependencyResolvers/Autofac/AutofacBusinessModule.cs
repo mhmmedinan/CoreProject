2cc157e003b5c9670cbdc21b37eeafa4a5e3abb2
mrm65 <minan.65@hotmail.com>
2022-05-14 13:41:17 +0300

    IoC Container

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<AboutManager>().As<IAboutService>().SingleInstance();
+            builder.RegisterType<EfAboutDal>().As<IAboutDal>().SingleInstance();
+
+            builder.RegisterType<ContactManager>().As<IContactService>().SingleInstance();
+            builder.RegisterType<EfContactDal>().As<IContactDal>().SingleInstance();
+
+            builder.RegisterType<ExperienceManager>().As<IExperienceService>().SingleInstance();
+            builder.RegisterType<EfExperienceDal>().As<IExperienceDal>().SingleInstance();
+
+            builder.RegisterType<FeatureManager>().As<IFeatureService>().SingleInstance();
+            builder.RegisterType<EfFeatureDal>().As<IFeatureDal>().SingleInstance();
+
+            builder.RegisterType<MessageManager>().As<IMessageService>().SingleInstance();
+            builder.RegisterType<EfMessageDal>().As<IMessageDal>().SingleInstance();
+
+            builder.RegisterType<PortfolioManager>().As<IPortfolioService>().SingleInstance();
+            builder.RegisterType<EfPortfolioDal>().As<IPortfolioDal>().SingleInstance();
+
+            builder.RegisterType<ServiceManager>().As<IServiceService>().SingleInstance();
+            builder.RegisterType<EfServiceDal>().As<IServiceDal>().SingleInstance();
+
+            builder.RegisterType<SkillManager>().As<ISkillService>().SingleInstance();
+            builder.RegisterType<EfSkillDal>().As<ISkillDal>().SingleInstance();
+
+            builder.RegisterType<SocialMediaManager>().As<ISocialMediaService>().SingleInstance();
+            builder.RegisterType<EfSocialMediaDal>().As<ISocialMediaDal>().SingleInstance();
+
+            builder.RegisterType<TestimonialsManager>().As<ITestimonialsService>().SingleInstance();
+            builder.RegisterType<EfTestimonialsDal>().As<ITestimonialsDal>().SingleInstance();
+        }
+    }
+}
