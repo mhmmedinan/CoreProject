698cf432fd1cfc287651ed1511c5bebcfb15a982
mrm65 <minan.65@hotmail.com>
2022-05-14 14:36:04 +0300

    FeatureList

--- MvcWebUI/ViewComponents/Feature/FeatureList.cs
+++ MvcWebUI/ViewComponents/Feature/FeatureList.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Abstract;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MvcWebUI.ViewComponents.Feature
 {
     public class FeatureList:ViewComponent
     {
+        private readonly IFeatureService _featureService;
+
+        public FeatureList(IFeatureService featureService)
+        {
+            _featureService = featureService;
+        }
 
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = _featureService.TGetAll();
+            return View(values.Data);
         }
     }
 }
