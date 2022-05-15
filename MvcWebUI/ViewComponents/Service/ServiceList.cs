3d1eec141476335b5802ea86db4e7242b5baac84
mrm65 <minan.65@hotmail.com>
2022-05-15 21:28:22 +0300

    ServiceList

---
+++ MvcWebUI/ViewComponents/Service/ServiceList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWebUI.ViewComponents.Service
+{
+    public class ServiceList:ViewComponent
+    {
+        private readonly IServiceService _serviceService;
+
+        public ServiceList(IServiceService serviceService)
+        {
+            _serviceService = serviceService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _serviceService.TGetAll();
+            return View(values.Data);
+        }
+    }
+}
