902a8285403fa524a0704a775baaf2812f0f929d
mrm65 <minan.65@hotmail.com>
2022-05-15 21:28:09 +0300

    PortfolioList

---
+++ MvcWebUI/ViewComponents/Portfolio/PortfolioList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWebUI.ViewComponents.Portfolio
+{
+    public class PortfolioList:ViewComponent
+    {
+        private readonly IPortfolioService _portfolioService;
+
+        public PortfolioList(IPortfolioService portfolioService)
+        {
+            _portfolioService = portfolioService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _portfolioService.TGetAll();
+            return View(values.Data);
+        }
+    }
+}
