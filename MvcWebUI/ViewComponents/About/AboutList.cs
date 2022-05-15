888106b7539cc80a0e16481bc7e0bf793593a273
mrm65 <minan.65@hotmail.com>
2022-05-15 21:27:55 +0300

    AboutList

---
+++ MvcWebUI/ViewComponents/About/AboutList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWebUI.ViewComponents.About
+{
+    public class AboutList:ViewComponent
+    {
+        private readonly IAboutService _aboutService;
+
+        public AboutList(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _aboutService.TGetAll();
+            return View(values.Data);
+        }
+    }
+}
