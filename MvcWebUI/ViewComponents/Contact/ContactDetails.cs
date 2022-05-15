5a0917a81c447527648e97f78f28ed2e0a0e1596
mrm65 <minan.65@hotmail.com>
2022-05-16 01:38:36 +0300

    ContactDetails

---
+++ MvcWebUI/ViewComponents/Contact/ContactDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWebUI.ViewComponents.Contact
+{
+    public class ContactDetails:ViewComponent
+    {
+        private readonly IContactService _contactService;
+
+        public ContactDetails(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _contactService.TGetAll();
+            return View(values.Data);
+        }
+    }
+}
