88c12831cd10ddb4a91d0b27fbbbde1cd4ac6f7e
mrm65 <minan.65@hotmail.com>
2022-05-15 21:27:23 +0300

    Concrete

---
+++ Entities/Concrete/Feature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Feature : IEntity
+    {
+        [Key]
+        public int  Id { get; set; }
+        public string Header { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+    }
+}
