d0447707ac21082ba47ca5ec4915ba4bbf238aff
mrm65 <minan.65@hotmail.com>
2022-05-15 21:28:39 +0300

    SkillList

---
+++ MvcWebUI/ViewComponents/Skill/SkillList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWebUI.ViewComponents.Skill
+{
+    public class SkillList:ViewComponent
+    {
+        private readonly ISkillService _skillService;
+
+        public SkillList(ISkillService skillService)
+        {
+            _skillService = skillService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _skillService.TGetAll();
+            return View(values.Data);
+        }
+    }
+}
