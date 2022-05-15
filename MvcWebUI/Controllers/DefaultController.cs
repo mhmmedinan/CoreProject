8e9f17f13472ca6547ab554739581ad92ab0f9f8
mrm65 <minan.65@hotmail.com>
2022-05-16 01:36:58 +0300

    Default Controller

--- MvcWebUI/Controllers/DefaultController.cs
+++ MvcWebUI/Controllers/DefaultController.cs
@@ -3,11 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
 
 namespace MvcWebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IMessageService _messageService;
+
+        public DefaultController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
         public IActionResult Index()
         {
             return View();
@@ -20,6 +28,21 @@ namespace MvcWebUI.Controllers
 
         public PartialViewResult NavbarPartial()
         {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public PartialViewResult SendMessage()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult SendMessage(Message message)
+        {
+            message.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            message.Status = true;
+            _messageService.TAdd(message);
             return PartialView();
         }
     }
