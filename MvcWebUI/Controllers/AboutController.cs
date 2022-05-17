c51f01fe093f00cc5e90cd2fcb0682cc0c85b8ab
mrm65 <minan.65@hotmail.com>
2022-05-18 01:28:51 +0300

    AboutController

---
+++ MvcWebUI/Controllers/AboutController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace MvcWebUI.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly IAboutService _aboutService;
+
+        public AboutController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Hakkımda";
+            ViewBag.v3 = "Hakkımda Sayfası";
+            var values = _aboutService.TGetById(1);
+            return View(values.Data);
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(About about)
+        {
+            _aboutService.TUpdate(about);
+            return RedirectToAction("Index", "Default");
+
+        }
+    }
+}
