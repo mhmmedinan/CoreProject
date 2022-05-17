e011706974399049b959078340c912ab8606c2f9
mrm65 <minan.65@hotmail.com>
2022-05-18 01:26:44 +0300

    ValidationRules

---
+++ Business/ValidationRules/PortfolioValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules
+{
+    public class PortfolioValidator:AbstractValidator<Portfolio>
+    {
+        public PortfolioValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Proje Adı Boş Geçilemez");
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Görsel alanı boş geçilemez");
+            RuleFor(x => x.ImageUrl2).NotEmpty().WithMessage("Görsel2 alanı boş geçilemez");
+            RuleFor(x => x.ProjectUrl).NotEmpty().WithMessage("Proje Url alanı boş geçilemez");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Fiyat alanı boş geçilemez");
+            RuleFor(x => x.Value).NotEmpty().WithMessage("Değer alanı boş geçilemez");
+            RuleFor(x => x.Platform).NotEmpty().WithMessage("Platform alanı boş geçilemez");
+            RuleFor(x => x.Status).NotEmpty().WithMessage("Durum alanı boş geçilemez");
+            RuleFor(x => x.Name).MinimumLength(5).WithMessage("Proje adı en az 5 karakterden oluşmak zorundadır");
+            RuleFor(x => x.Name).MaximumLength(100).WithMessage("Proje adı en fazla 100 karakterden oluşmak zorundadır");
+
+
+        }
+    }
+}
