28f1aac85a0cb224cbe6e80d845f1606a0c0e926
mrm65 <minan.65@hotmail.com>
2022-05-18 01:29:03 +0300

    FeatureController

--- MvcWebUI/Controllers/FeatureController.cs
+++ MvcWebUI/Controllers/FeatureController.cs
@@ -3,14 +3,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
 
 namespace MvcWebUI.Controllers
 {
     public class FeatureController : Controller
     {
+        private readonly IFeatureService _featureService;
+
+        public FeatureController(IFeatureService featureService)
+        {
+            _featureService = featureService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Öne Çıkanlar";
+            ViewBag.v3 = "Öne Çıkan Sayfası";
+            var values = _featureService.TGetById(1);
+            return View(values.Data);
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(Feature feature)
+        {
+            _featureService.TUpdate(feature);
+            return RedirectToAction("Index","Default");
+
         }
     }
 }
