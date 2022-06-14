408bcac564d2763e3f4073673916506110b17040
mrm65 <minan.65@hotmail.com>
2022-06-14 21:49:11 +0300

    Updated Project

---
+++ MvcWebUI/Controllers/ContactSubPlaceController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace MvcWebUI.Controllers
+{
+    public class ContactSubPlaceController : Controller
+    {
+        private readonly IContactService _contactService;
+
+        public ContactSubPlaceController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values = _contactService.TGetById(1);
+            return View(values.Data);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            _contactService.TUpdate(contact);
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
---
+++ MvcWebUI/Controllers/TestimonialController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace MvcWebUI.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly ITestimonialService _testimonialService;
+
+        public TestimonialController(ITestimonialService testimonialService)
+        {
+            _testimonialService = testimonialService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _testimonialService.TGetAll();
+            return View(values.Data);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var values = _testimonialService.TGetById(id);
+            _testimonialService.TDelete(values.Data);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var values = _testimonialService.TGetById(id);
+            return View(values.Data);
+        }
+
+
+        [HttpPost]
+        public IActionResult Update(Testimonial testimonial)
+        {
+            _testimonialService.TUpdate(testimonial);
+            return RedirectToAction("Index");
+        }
+    }
+}
